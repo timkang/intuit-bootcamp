8dc46dae8daee1ad2d32b11778de2764502f8a57
Eric W. Greene <eric@trainingfordevelopers.com>
2015-04-30 18:15:22 +0100

    Added widget content

--- app/www/js/dojo/dojo-config.js
+++ app/www/js/dojo/dojo-config.js
@@ -1,10 +1,14 @@
 var dojoConfig = {
 	async: true,
-	cacheBust: true,
+	//cacheBust: true,
 	isDebug: true,
-	//parseOnLoad: true,
-	//deps: ["dojo/parser"],
+	parseOnLoad: true,
+	deps: ["dojo/parser"],
 	packages: [
-		{ name: "myapp", location: "../../js/myapp" }
+		{ name: "myapp", location: "../../js/myapp" },
+		{ name: "jquery", location: "../jquery/dist", main: "jquery" },
+		{ name: "underscore", location: "../underscore", main: "underscore" },
+		{ name: "backbone", location: "../backbone", main: "backbone" },
+		{ name: "marionette", location: "../backbone.marionette", main: "backbone.marionette" }
 	]
 };
